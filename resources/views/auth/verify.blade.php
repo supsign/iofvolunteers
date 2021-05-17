1a129a2854240b7933308a816810951f9dff626c
Fabrizio Cocco <fabrizio.cocco@supsign.ch>
2021-05-17 15:28:37 +0200

    no message

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+<section class="default">
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -25,4 +27,5 @@
         </div>
     </div>
 </div>
+</section>
 @endsection
