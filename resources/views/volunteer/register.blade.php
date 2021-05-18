7e3068834468ec61d756cc29707c42239aa603aa
Florian Ratz <florian.ratz@supsign.ch>
2021-05-18 15:32:54 +0200

    fix label mismatch

--- resources/views/volunteer/register.blade.php
+++ resources/views/volunteer/register.blade.php
@@ -95,7 +95,7 @@
                             @foreach($disciplines AS $discipline)
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" value="1" id="discipline_{{ $discipline->snakeCaseName }}" name="discipline[{{ $discipline->id }}]">
-                                    <label class="form-check-label" for="{{ $discipline->snakeCaseName }}">
+                                    <label class="form-check-label" for="discipline_{{ $discipline->snakeCaseName }}">
                                         {{ $discipline->name }}
                                     </label>
                                 </div>
