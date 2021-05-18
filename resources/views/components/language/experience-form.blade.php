dc738949dffc3fe44784f1a1cdbb5a1630eed7e1
Fabrizio Cocco <fabrizio.cocco@supsign.ch>
2021-05-18 11:40:58 +0200

    no message

---
+++ resources/views/components/language/experience-form.blade.php
@@ -0,0 +1,38 @@
+<div class="formSection">
+    <h3 class="formSectionTitle">
+        5. Languages spoken
+        <div class="warn"> (required, even if only listed in "other")</div>
+    </h3>
+
+@foreach($languages as $l)
+<div class="form-group form-inline">
+    <div class="form-check">
+        <label class="form-check-label">
+            {{ $l->name }}
+        </label>
+    </div>
+@foreach ($languageProficiencies as $lp )
+<div class="form-check">
+    <input class="form-check-input" type="radio" name="language[{{ $l->id }}]" value="{{ $lp->id }}" id="languages_{{ $l->id }}_{{ $lp->id }}"
+    @if($lp->name === 'none' && !isset($langSelection))
+    checked="checked"    
+    @endif
+    >
+    <label class="form-check-label" for="languages_{{ $l->id }}_{{ $lp->id }}">
+{{$lp->name}}
+</label>
+</div>
+@endforeach
+
+</div>
+@endforeach
+
+
+    <div class="form-group">
+        <input id="field_languagesOther" placeholder=" " type="text" name="other_languages" value="" size="30">
+        <label class="formGroupLabel"  for="field_languagesOther">Other languages?</label>
+        <div class="font-italic">
+            State each language and level, separated by commas below...
+        </div>
+    </div>
+</div>
