bce6c39d76f05a349a54cb35829fb44851d6fc43
Florian Ratz <florian.ratz@supsign.ch>
2021-05-18 11:26:18 +0200

    no message

--- database/migrations/2021_05_18_090854_create_duties_table.php
+++ database/migrations/2021_05_18_090854_create_duties_table.php
@@ -16,7 +16,7 @@ class CreateDutiesTable extends Migration
         Schema::create('duties', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->timestamps();
+            $table->timestampsTz();
         });
 
         Artisan::call('db:seed', [
@@ -24,9 +24,21 @@ class CreateDutiesTable extends Migration
             '--force' => true
         ]);
 
+        Schema::create('duty_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestampsTz();
+        });
+
+        Artisan::call('db:seed', [
+            '--class' => DutyTypeSeeder::class,
+            '--force' => true
+        ]);
+
         Schema::create('duty_models', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('duty_id')->constrained('duties');
+            $table->foreignId('duty_id')->constrained();
+            $table->foreignId('duty_type_id')->constrained();
             $table->unsignedBigInteger('duty_model_id');
             $table->string('duty_model_type');
             $table->timestampsTz();
