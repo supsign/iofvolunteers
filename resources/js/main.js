137ca398e3d9fce9c89b97a30d61565356f3e1cb
Sil3nce72 <nb.guen@hotmail.com>
2021-08-10 10:15:43 +0200

    JS und Klassen angepasst

--- resources/js/main.js
+++ resources/js/main.js
@@ -21,3 +21,25 @@ jQuery(document).ready(function($){
         }
     });
 });
+
+$(document).ready(function(){
+    $('#AnyId').on('click',function(){
+        if(this.checked){
+            $('.checkbox').each(function(){
+                this.checked = true;
+            });
+        }else{
+            $('.checkbox').each(function(){
+                this.checked = false;
+            });
+        }
+    });
+    
+    $('.checkbox').on('click',function(){
+        if($('.checkbox:checked').length == $('.checkbox').length){
+            $('#AnyId').prop('checked',true);
+        }else{
+            $('#AnyId').prop('checked',false);
+        }
+    });
+});
