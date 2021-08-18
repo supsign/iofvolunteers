0627a45d23e29f3936f2d12a6321511dadca9668
Florian Ratz <florian.ratz@supsign.ch>
2021-08-18 08:51:06 +0200

    no message

---
+++ app/Http/Requests/VolunteerEdit.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VolunteerEdit extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:191',
+            'country_id' => 'int',
+            'email' => 'required|email:rfc,dns|max:191',
+            'gender_id' => 'int',
+            'birthdate' => 'required|date',
+            'nickname' => 'max:191',
+            'driving_licence' => 'required|boolean',
+            'discipline' => '',
+            'ol_duration' => 'int',
+            'club' => 'max:191',
+            'o_experience' => '',
+            'language' => '',
+            'continent' => '',
+            'work_duration' => 'int|nullable',
+            'skill' => '',
+            'skill_teaching' => '',
+            'skill_mapping' => '',
+            'skill_coaching' => '',
+            'skill_event_organising' => '',
+            'skill_it' => '',
+            'skill_other' => '',
+            'o_work_expirence' => '',
+            'duty' => '',
+            'help' => '',
+            'expectation' => '',
+        ];
+    }
+}
