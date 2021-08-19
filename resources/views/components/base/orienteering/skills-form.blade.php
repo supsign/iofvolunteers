6a79d0f868d665d4f3a49e47a3714a6cbba5b7ff
Sil3nce72 <nb.guen@hotmail.com>
2021-08-19 16:30:35 +0200

    vor neuen Anpassungen

--- resources/views/components/base/orienteering/skills-form.blade.php
+++ resources/views/components/base/orienteering/skills-form.blade.php
@@ -54,6 +54,9 @@
             </div>
         </div>
     @endforeach
-    
-</div>
 
+    <div class="form-group">
+        <textarea placeholder=" " rows="2" cols="30" name="skill_other" id="skill_other" value="">{{ old('skill_other') ?? $volunteer?->skill_other }}</textarea>
+        <label class="formGroupLabel" for="skill_other">* Other skills? Please explain...</label>
+    </div>
+</div>
