94f41d96300e170691fc32850cf27c7ba09905ac
Sil3nce72 <nb.guen@hotmail.com>
2021-08-19 16:28:30 +0200

    vor neuen Anpassungen

--- resources/views/volunteer/register.blade.php
+++ resources/views/volunteer/register.blade.php
@@ -143,7 +143,14 @@
                         </div>
                     </div>
 
-                    <x-volunteer.skills-form/>
+                    <x-volunteer.skills-form>
+                        <x-slot name="title">
+                            8. Skills
+                        </x-slot>
+                        <x-slot name="subtitle">
+                            <div class="warn"> (Please tick all relevant to you. Details are required if skill is ticked)</div>
+                        </x-slot>
+                    </x-base.skills-form>
 
                     <div class="formSection">
                         <h3 class="formSectionTitle">
