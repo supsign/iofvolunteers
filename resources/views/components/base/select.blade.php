ce21a4dcb4a279604d21bc726fcb7f7825e0ca0b
Sil3nce72 <nb.guen@hotmail.com>
2021-08-20 11:29:09 +0200

    select angefangen

--- resources/views/components/base/select.blade.php
+++ resources/views/components/base/select.blade.php
@@ -1,14 +1,8 @@
 <div class="form-group">
-    <div class="warn">Country</div>
-    <select type="text" name="country_id" id="country_id" size="1" value="" required>
-        @php
-            if(isset($volunteer)) {
-                $oldCountry= old('country_id') ?: $volunteer->country_id;
-            }
-            else {
-                $oldCountry= old('country_id');
-            }
-        @endphp     
+    <select 
+        id="{{ $attributes->get('id') }}" 
+        name="{{ $attributes->get('name') }}" 
+    >
         @if(!$oldCountry)
             <option selected disabled>Country</option>
         @endif
@@ -16,9 +10,10 @@
             <option value="{{ $country->id }}" @if($oldCountry == $country->id) selected @endif>{{ ucfirst($country->name) }}</option>
         @endforeach
     </select>
-    <img for="country" class="selectArr selectArrComponents" src="{{ asset('images/selectArr.svg') }}" alt="" />
+        
+
     <div class="mt-3">
-        @foreach ($errors->get('country_id') as $message)
+        @foreach ($errors->get($attributes->get('name')) as $message)
             <div class="alert alert-danger">{{ $message }} </div>
         @endforeach
     </div>
