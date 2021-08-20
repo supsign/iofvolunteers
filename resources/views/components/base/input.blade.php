6925d60ad4b0238ddd7c63397e218076b9ea513b
Florian Ratz <florian.ratz@supsign.ch>
2021-08-20 09:44:24 +0200

    use attributes instead?

---
+++ resources/views/components/base/input.blade.php
@@ -0,0 +1,12 @@
+<input 
+    id="field_{{ $attributes->get('name') }}"
+    type={{ $attributes->get('type') }} 
+    name={{ $attributes->get('name') }} 
+    value="{{ $attributes->get('value') }}"
+>
+<label class="formGroupLabel" for="field_field_{{ $attributes->get('name') }}">field_{{ $attributes->get('label') }}</label>
+<div class="mt-3">
+    @foreach ($errors->get($attributes->get('name')) as $message)
+        <div class="alert alert-danger">{{ $message }} </div>
+    @endforeach
+</div>
