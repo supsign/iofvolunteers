24c1d2afde2759fc63307f01307b96df345f83ff
Sil3nce72 <nb.guen@hotmail.com>
2021-08-20 09:55:47 +0200

    label input angepasst

--- resources/views/components/base/input.blade.php
+++ resources/views/components/base/input.blade.php
@@ -4,7 +4,7 @@
     name={{ $attributes->get('name') }} 
     value="{{ $attributes->get('value') }}"
 >
-<label class="formGroupLabel" for="field_{{ $attributes->get('name') }}">field_{{ $attributes->get('label') }}</label>
+<label class="formGroupLabel" for="field_{{ $attributes->get('name') }}">{{ $attributes->get('label') }}</label>
 <div class="mt-3">
     @foreach ($errors->get($attributes->get('name')) as $message)
         <div class="alert alert-danger">{{ $message }} </div>
