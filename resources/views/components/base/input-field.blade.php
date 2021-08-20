48d8dd9f498fd015c3e8b4b216d044d4ec935e35
Sil3nce72 <nb.guen@hotmail.com>
2021-08-20 09:37:25 +0200

    Typo gefixt

--- resources/views/components/base/input-field.blade.php
+++ resources/views/components/base/input-field.blade.php
@@ -2,7 +2,7 @@
     <input id="field_name" placeholder=" " type={{ $type }} name={{ $name }} value=" {{ $value }}" size="15" required>
     <label class="formGroupLabel" for="field_name"> {{ $label }}</label>
     <div class="mt-3">
-        @foreach ($errors->get('name') as $message)
+        @foreach ($errors->get($name) as $message)
             <div class="alert alert-danger">{{ $message }} </div>
         @endforeach
     </div>
