df49a764d7d4d0ed6c4624581abc4f8a84d9598c
Sil3nce72 <nb.guen@hotmail.com>
2021-08-20 09:34:04 +0200

    input-blade und component erstellt

---
+++ resources/views/components/base/input-field.blade.php
@@ -0,0 +1,9 @@
+<div class="form-group">
+    <input id="field_name" placeholder=" " type={{ $type }} name={{ $name }} value=" {{ $value }}" size="15" required>
+    <label class="formGroupLabel" for="field_name"> {{ $label }}</label>
+    <div class="mt-3">
+        @foreach ($errors->get('name') as $message)
+            <div class="alert alert-danger">{{ $message }} </div>
+        @endforeach
+    </div>
+</div>
