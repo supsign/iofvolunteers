7f2fa33af4fd681ad6ceedafc0fce93a1ac2f2eb
Florian Ratz <florian.ratz@supsign.ch>
2021-08-23 16:36:54 +0200

    checkbox mit checked, wip

--- resources/views/components/base/checkbox.blade.php
+++ resources/views/components/base/checkbox.blade.php
@@ -3,6 +3,7 @@
         type="checkbox"
         id="{{ $attributes->get('name') }}"
         value="{{ old($attributes->get('name')) ?? $attributes->get('value') }}"
+        @if(old($attributes->get('name')) || $attributes->get('value')) checked @endif
         {{ $attributes->filter(fn ($value, $key) => !in_array($key, ['label'])) }}
     />
     <label class="form-check-label" for="{{ $attributes->get('name') }}">
