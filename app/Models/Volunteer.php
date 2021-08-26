36400f15caa58b364c865507b17a38ffcfcfe785
Sil3nce72 <nb.guen@hotmail.com>
2021-08-26 08:26:01 +0200

    doppelte Funktion rausgenommen

--- app/Models/Volunteer.php
+++ app/Models/Volunteer.php
@@ -115,13 +115,5 @@ class Volunteer extends BaseModel
             }
         );
     }
-
-    public function hasDuty(Duty $duty, DutyType $dutyType)
-    {
-        return $this->dutyVolunteer->contains(
-            function ($dutyVolunteer) use ($duty, $dutyType) {
-                return $duty->id === $dutyVolunteer->duty_id && $dutyType->id === $dutyVolunteer->duty_type_id;
-            }
-        );
-    }
+    
 }
