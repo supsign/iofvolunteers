2ac9745fcc4638cc899637404bbd6ccfbd72fbbd
Florian Ratz <florian.ratz@supsign.ch>
2021-08-27 08:44:09 +0200

    is_null check für select, weil null == 0 is true ...

--- resources/views/components/base/select.blade.php
+++ resources/views/components/base/select.blade.php
@@ -11,7 +11,7 @@
         @endif
 
         @foreach($options AS $option)
-            <option value="{{ $option->id }}" @if(old($attributes->get('name')) == $option->id OR $value && $value->id === $option->id) selected @endif>{{ $option->name }}</option>
+            <option value="{{ $option->id }}" @if(!is_null(old($attributes->get('name'))) && old($attributes->get('name')) == $option->id OR $value && $value->id === $option->id) selected @endif>{{ $option->name }}</option>
         @endforeach
     </select>
 
