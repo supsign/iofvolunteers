3b96ff557cc8ebe2feba47351927ba3234bc92f3
Fabrizio Cocco <fabrizio.cocco@supsign.ch>
2021-08-28 21:54:32 +0200

    typo und fixes

--- resources/views/components/form/section.blade.php
+++ resources/views/components/form/section.blade.php
@@ -1,11 +1,11 @@
 <div class="formSection">
     @isset($title)
-        <h3 class="formSectionTitle">{{ $title }}      
-        @isset($subtitle)
-            <div class="formSubtitle2">
-                {{ $subtitle }}
-            </div>
-        @endisset    
+        <h3 class="formSectionTitle">{{ $title }}
+            @isset($subtitle)
+                <div class="formSubtitle2">
+                    {{ $subtitle }}
+                </div>
+            @endisset
         </h3>
     @endisset
 
