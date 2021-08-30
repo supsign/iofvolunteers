ef526a23b171c5bbc8fb7eb2cfa6dc0f6103ea52
Sil3nce72 <nb.guen@hotmail.com>
2021-08-30 14:20:52 +0200

    kleine Anpassungen

--- resources/views/project/list.blade.php
+++ resources/views/project/list.blade.php
@@ -22,23 +22,22 @@
                         <td class="big-desc">Organisation Name</td>
                         <td class="big-desc">Project Contact</td>
                         <td class="big-desc"></td>
-                    </tr>
-                    
-                        @foreach ($projects as $project)
-                            <tr>
-                                <td class="desc">
-                                    <a href="{{ route('project.show', $project ) }}">
-                                        {{ $project->name }}
-                                    </a>
-                                </td>
-                                <td class="desc">{{ $project->organisation_name }}</td>
-                                <td class="desc">{{ $project->contact }}</td>
-                                <td class="desc">
-                                    <input type="button" onclick="location.href='{{ route('project.edit', $project ) }}'" value="Edit"/>
-                                    <input type="button" onclick="location.href='{{ route('project.delete', $project ) }}" value="Delete"/>
-                                </td>
-                            </tr>
-                        @endforeach                
+                    </tr>                 
+                    @foreach ($projects as $project)
+                        <tr>
+                            <td class="desc">
+                                <a href="{{ route('project.show', $project ) }}">
+                                    {{ $project->name }}
+                                </a>
+                            </td>
+                            <td class="desc">{{ $project->organisation_name }}</td>
+                            <td class="desc">{{ $project->contact }}</td>
+                            <td class="desc">
+                                <input type="button" onclick="location.href='{{ route('project.edit', $project ) }}'" value="Edit"/>
+                                <input type="button" onclick="location.href='{{ route('project.delete', $project ) }}" value="Delete"/>
+                            </td>
+                        </tr>
+                    @endforeach                
                 </tbody>
             </table>
         </div>
