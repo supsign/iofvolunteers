103959c31e938eefb419dfb9faf999db1318cfeb
Sil3nce72 <nb.guen@hotmail.com>
2021-08-30 11:52:00 +0200

    email-blade angepasst

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -18,31 +18,24 @@
 
                             <form method="POST" action="{{ route('password.email') }}">
                                 @csrf
+                                <x-form.section>
+                                    <div class="row justify-content-md-center">
 
-                                <div class="form-group row">
-                                    <label for="email"
-                                           class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                                    <div class="col-md-6">
-                                        <input id="email" type="email"
-                                               class="form-control @error('email') is-invalid @enderror" name="email"
-                                               value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                        @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
+                                        <div class="col-md-7">
+                                            <x-base.input name="email" type="email" label="{{ __('E-Mail Address') }}"
+                                                            value="{{ old('email') }}" required
+                                                            autocomplete="email" autofocus/>
+                                        </div>
                                     </div>
-                                </div>
 
-                                <div class="form-group row mb-0">
-                                    <div class="col-md-6 offset-md-4">
-                                        <button type="submit" class="btn btn-primary">
-                                            {{ __('Send Password Reset Link') }}
-                                        </button>
+                                    <div class="row justify-content-md-center">
+                                        <div class="col-md-7">
+                                            <button type="submit" class="btn btn-primary">
+                                                {{ __('Send Password Reset Link') }}
+                                            </button>
+                                        </div>
                                     </div>
-                                </div>
+                                </x-form.section>
                             </form>
                         </div>
                     </div>
