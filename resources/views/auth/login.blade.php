cffde0b2b1182e5ce31e76f285fb3c75fe4e88e0
Sil3nce72 <nb.guen@hotmail.com>
2021-08-30 10:56:44 +0200

    typo gefixt

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                     <div class="card">
                         <div class="card-header">{{ __('Login') }}</div>
 
-                        <div class="card-body ">
+                        <div class="card-body">
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
                                 <x-form.section>
