1b9b80d9e9da404f9c4db2a941f99c37af15a775
Sil3nce72 <nb.guen@hotmail.com>
2021-08-30 10:07:35 +0200

    login auf komponenten umgebaut

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,59 +8,42 @@
                     <div class="card">
                         <div class="card-header">{{ __('Login') }}</div>
 
-                        <div class="card-body">
+                        <div class="card-body ">
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
-
-                                <div class="form-group row">
-                                    <label for="email"
-                                           class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                                    <div class="col-md-6">
-                                        <input id="email" type="email"
-                                               class="form-control @error('email') is-invalid @enderror" name="email"
-                                               value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                        @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
+                                <x-form.section>
+                                <div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <x-base.input name="email" type="email" label="{{ __('E-Mail Address:') }}"
+                                                    value="{{ old('email') }}" required autocomplete="email" autofocus/>
+                                        </div>
                                     </div>
-                                </div>
 
-                                <div class="form-group row">
-                                    <label for="password"
-                                           class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                    <div class="col-md-6">
-                                        <input id="password" type="password"
-                                               class="form-control @error('password') is-invalid @enderror"
-                                               name="password" required autocomplete="current-password">
-
-                                        @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
+                                    <div class="row">
+                                        <div class="col-md-6">                                                                       
+                                            <x-base.input name="password" type="password" label="{{ __('Password:') }}"
+                                                    value="{{ old('password') }}" required autocomplete="current-password"/>
+                                        </div>
                                     </div>
-                                </div>
 
-                                <div class="form-group row">
-                                    <div class="col-md-6 offset-md-4">
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" name="remember"
-                                                   id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <div class="form-group row">
+                                        <div class="col-md-6">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" name="remember"
+                                                    id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                            <label class="form-check-label" for="remember">
-                                                {{ __('Remember Me') }}
-                                            </label>
+                                                <label class="form-check-label" for="remember">
+                                                    {{ __('Remember Me') }}
+                                                </label>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                            </x-form.section>
 
                                 <div class="form-group row mb-0">
-                                    <div class="col-md-8 offset-md-4">
+                                    <div class="col-md-8">
                                         <button type="submit" class="btn btn-primary">
                                             {{ __('Login') }}
                                         </button>
