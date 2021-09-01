9293cf4661362fea464ecdae36e728430080d7a5
Sil3nce72 <nb.guen@hotmail.com>
2021-09-01 17:02:54 +0200

    codesmells

--- resources/views/project/preview.blade.php
+++ resources/views/project/preview.blade.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="titleWrap">
                 <h1 class="title pb-0"><img class="title-icon" src="{{ asset('images/icon-search1.svg') }}" width="65"
-                                            height="65"> Project Details</h1>
+                                            height="65" alt="search icon"> Project Details</h1>
             </div>
             <input type="button" class="mb-3" onclick="window.history.go(-1); return false;" value="Back to project list"/>
 
