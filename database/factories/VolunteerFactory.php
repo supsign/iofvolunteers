cb40be4c6ef3444deffd5559b776f592b8ab680f
Shift <shift@laravelshift.com>
2022-02-14 06:00:00 +0000

    Remove unnecessary `$model` property

--- database/factories/VolunteerFactory.php
+++ database/factories/VolunteerFactory.php
@@ -8,13 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class VolunteerFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Volunteer::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
